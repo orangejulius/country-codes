84293b263c3b124df1614e9af69b540fa6f4bb72
Julian Simioni <julian@simioni.org>
2015-10-09 17:55:05 -0400

    Quick test for all country codes

--- import/index.js
+++ import/index.js
@@ -7,12 +7,23 @@ var baseUrl = 'https://raw.githubusercontent.com/whosonfirst/whosonfirst-data/ma
 var countries = [];
 
 request(baseUrl + 'meta/wof-country-latest.csv').pipe(csv()).on('data', function(row) {
-  console.log(row.name);
 
   var JSONStream = require('JSONStream');
 
   request(baseUrl + 'data/' + row.path).pipe(JSONStream.parse()).on('error', function(err) { console.log(err); }).on('data', function(data) {
-    console.log(data.properties['ne:iso_a2']);
+    var a3 = data.properties['ne:iso_a3'] || data.properties['qs:adm0_a3'];
+    var a2 = data.properties['iso:country'] || data.properties['ne:iso_a2'];
+
+    if (a3 ==='SSD') {
+      console.log('south sudan found');
+    }
+
+    if (!a3) {
+      console.log(data.properties['wof:name'] + '( wof:id ' + data.properties['wof:id'] + ') had no alpha3 code');
+    }
+    if (!a2) {
+      console.log(data.properties['wof:name'] + '( wof:id ' + data.properties['wof:id'] + ') had no alpha2 code');
+    }
   });
 
 
